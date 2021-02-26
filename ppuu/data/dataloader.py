ef550b794692c7a432aafbe2ff7ef22726052249
Vlad <us441@nyu.edu>
2021-02-26 17:18:30 -0500

    remove debug prints

--- ppuu/data/dataloader.py
+++ ppuu/data/dataloader.py
@@ -321,7 +321,6 @@ class EvaluationDataset(torch.utils.data.Dataset):
                 test=splits.get("test_indx"),
             )
 
-        print('first 10 elements of test split', self.splits['test'][:10])
 
         car_sizes_path = os.path.join(data_dir, "car_sizes.pth")
         self.car_sizes = torch.load(car_sizes_path)
@@ -373,12 +372,10 @@ class EvaluationDataset(torch.utils.data.Dataset):
             yield self[i]
 
     def __getitem__(self, i):
-        print('getting item', i)
         car_info = self.get_episode_car_info(self.splits[self.split][i])
         return car_info
 
     def get_episode_car_info(self, episode):
-        print('getting episode', episode)
         splits = self.ids[episode].split("/")
         time_slot_str = splits[-2]
         car_id = int(re.findall(r"car(\d+).pkl", splits[-1])[0])
@@ -389,8 +386,6 @@ class EvaluationDataset(torch.utils.data.Dataset):
         }
         time_slot = data_files_mapping[time_slot_str]
         car_size = self.car_sizes[time_slot_str][car_id]
-        print('time slot is', time_slot)
-        print('car_id is', car_id)
         result = dict(
             time_slot=time_slot,
             time_slot_str=time_slot_str,
